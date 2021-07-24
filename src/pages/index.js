5e69ee7839f99c44ec0b18552b6cc980eade0857
lectri <cmade2006@gmail.com>
2021-07-24 17:25:27 -0400

    No heading or paragraph centering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import * as React from "react";
-import Header from "../components/header";
-import { Description } from "../css/index.module.css";
+import IndexHeader from "../components/headers/index-header";
+
 
 const IndexPage = () => {
   return (
     <div className='IndexPage'>
-      <Header pageTitle='Home Page'/>
-      <p className={Description}>Welcome to the Splatoon 2 Armory. We're all about Splatoon 2's guns and fighting tatics. Check out the links in the header to learn more.</p>
+      <IndexHeader/>
+      <h1>Home</h1>
+      <p>Welcome to the Splatoon 2 Armory. We're all about Splatoon 2's guns and fighting tatics. 
+      Check out the links in the header to learn more.
+      </p>
     </div>
   );
 }
