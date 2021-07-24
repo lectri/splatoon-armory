b05acba651a093cd2203c422c3714d60a3b9d6da
lectri <cmade2006@gmail.com>
2021-07-24 17:28:41 -0400

    New weapon classes main page

---
+++ src/components/headers/weapon-class-header.js
@@ -0,0 +1,30 @@
+import { Link } from "gatsby";
+import React from "react";
+import GenericHeader from "./generic-header";
+import {
+    HeaderElementList,
+    HeaderElement
+
+} from '../../css/header.module.css';
+
+// The header that will be used at the top of every page in the weapon class section
+const WeaponHeader = () => {
+    return (
+        <div className='WeaponHeader'>
+            <GenericHeader>
+                <ul className={HeaderElementList}>
+                    <li className={HeaderElement}><Link>Shooter</Link></li>
+                    <li className={HeaderElement}><Link>Roller</Link></li>
+                    <li className={HeaderElement}><Link>Charger</Link></li>
+                    <li className={HeaderElement}><Link>Slosher</Link></li>
+                    <li className={HeaderElement}><Link>Splatling</Link></li>
+                    <li className={HeaderElement}><Link>Dualies</Link></li>
+                    <li className={HeaderElement}><Link>Brellas</Link></li>
+                </ul>
+            </GenericHeader>
+        </div>
+    )
+    
+}
+
+export default WeaponHeader;
---
+++ src/pages/weapon-classes/info.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import WeaponHeader from "../../components/headers/weapon-class-header";
+
+const WeaponClasses = () => {
+  return (
+    <div>
+      <WeaponHeader/>
+      <h1>Weapon Classes</h1>
+      <p>This section of the site is about the different types of weapon classes and what they can do. 
+        Click on the header to read about them.
+        </p>
+    </div>
+    
+
+  )
+}
+
+export default WeaponClasses;
