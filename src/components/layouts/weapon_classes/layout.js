f834458ac7b0c686ecb85f2a64f885ef64df3c72
lectri <cmade2006@gmail.com>
2021-09-06 15:19:28 -0400

    Centered Content

--- src/components/layouts/weapon_classes/layout.js
+++ src/components/layouts/weapon_classes/layout.js
@@ -1,20 +1,24 @@
 import * as React from 'react';
 import WeaponHeader from '../../headers/weapon-class-header';
-import { weaponImage } from '../../../css/layout.module.css';
+import { weaponImage, weaponInfo } from '../../../css/layout.module.css';
 import * as Style from '../../../css/main.module.css';
 
 // Layout that will be used in every Weapon Class page
 const Layout = ({headingTitle, description, image, imageAlt, stats, children}) => {
     return (
         <div>
-            <WeaponHeader/> 
-            <h1 className={Style.heading}>{headingTitle}</h1>
-            <p className={Style.description}>{description}</p>
-            <div className={Style.children}>
-                <h3>Stats</h3>
-                {stats}
-                {children}
+            <WeaponHeader/>
+            <div className={weaponInfo}>
+                <h1 className={Style.heading}>{headingTitle}</h1>
+                <p className={Style.description}>{description}</p>
+                <div className={Style.children}>
+                    <div className={Style.stats}>
+                    <h3 className={Style.statsHeading}>Stats</h3>
+                        {stats}
+                    </div>
+                    {children}
             </div>
+        </div>
             
             <img src={image} alt={imageAlt} className={weaponImage}></img>
         </div>  
